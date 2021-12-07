d903ba5cd82501ec26211548f34385ff07859999
GiulianeEC <giuliane.correa@aluno.ufop.edu.br>
2021-12-07 18:58:43 -0300

    fim

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
-import { createServer,Model} from 'miragejs';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
 
 createServer({
-  models:{
-    transition: Model,
-  }, 
+  models: {
+    transaction: Model,
+  },
 
-  seeds(server){
+  seeds(server) {
     server.db.loadData({
-      transitions:[
+      transactions: [
         {
-          id:1,
-          title: 'Freelancer de website',
-          type:'deposit',
-          category:'Dev',
-          Amount:6000,
-          createdAt:new Date('2021-02-12 09:00:00'),
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
         },
         {
-        id:2,
-        title: 'Aluguel',
-        type:'withdraw',
-        category:'casa',
-        Amount:1100,
-        createdAt:new Date('2021-02-14 07:00:00'),
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
         }
       ],
     })
   },
 
-  routes(){
+  routes() {
     this.namespace = 'api';
 
-    this.get('/transactions',()=>{
-      return this.schema.all('transaction')
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
     })
 
-    this.post('/transactions',(schema,request)=>{
-      const data = JSON.parse(request.requestBody)
-      
-      return schema.create('transaction',data)
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
@@ -52,5 +52,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
